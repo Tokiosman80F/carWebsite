804b0796638a5e61159af1ab6dfad5aca07200cd
Tokiosman80F <tokiosman96@gmail.com>
2023-10-24 22:03:40 +0600

    deleting is working and update the table in real time

--- src/pages/BookingItems/BookingItems.jsx
+++ src/pages/BookingItems/BookingItems.jsx
@@ -5,7 +5,26 @@ import BookingRow from "./BookingRow";
 const BookingItems = () => {
   const { user } = useContext(AuthContext);
   const url = `http://localhost:3000/booking?email=${user?.email}`;
+  
   const [bookings, setBookings] = useState([]);
+
+  const handleDeleteItem = (id) => {
+    console.log(id);
+    fetch(`http://localhost:3000/booking/${id}`, {
+        method:"DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if(data.deletedCount>0)
+        {
+            confirm("Wanna a delete the item?")
+            const remainingItems=bookings.filter(item=>item._id!==id)
+            setBookings(remainingItems)
+        }
+      });
+  };
+  
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
@@ -14,7 +33,9 @@ const BookingItems = () => {
         console.log(data);
       });
   }, []);
-
+  
+  
+  
   return (
     <div>
       <h1 className="text-3xl text-center">Booking Items</h1>
@@ -35,7 +56,7 @@ const BookingItems = () => {
               <th>Status</th>
             </tr>
           </thead>
-          <tbody>{bookings.map(booking=><BookingRow key={booking._id} booking={booking}></BookingRow>)}</tbody>
+          <tbody>{bookings.map(booking=><BookingRow key={booking._id} booking={booking} handleDeleteItem={handleDeleteItem}></BookingRow>)}</tbody>
         </table>
       </div>
     </div>
--- src/pages/BookingItems/BookingRow.jsx
+++ src/pages/BookingItems/BookingRow.jsx
@@ -1,37 +1,54 @@
-import React from 'react';
+const BookingRow = ({ booking,handleDeleteItem }) => {
+  const { _id, name, price, img, date } = booking;
 
-const BookingRow = ({booking}) => {
-    const {name,price,img,date}=booking
-    return (
-        <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          <div className="flex items-center space-x-3">
-            <div className="avatar">
-              <div className=" w-24 h-24 rounded">
-                <img src={img} />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">{name}</div>
-              {/* <div className="text-sm opacity-50">United States</div> */}
+  return (
+    <tr>
+      <th>
+        <label>
+          <button
+            className="btn btn-circle btn-sm btn-outline"
+            onClick={() => handleDeleteItem(_id)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </label>
+      </th>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div className="avatar">
+            <div className=" w-24 h-24 rounded">
+              <img src={img} />
             </div>
           </div>
-        </td>
-        <td>
-          {price}
-          <br/>
-        </td>
-        <td>{date}</td>
-        <th>
-          <button className="btn btn-error">Pending</button>
-        </th>
-      </tr>
-    );
+          <div>
+            <div className="font-bold">{name}</div>
+            {/* <div className="text-sm opacity-50">United States</div> */}
+          </div>
+        </div>
+      </td>
+      <td>
+        {price}
+        <br />
+      </td>
+      <td>{date}</td>
+      <th>
+        <button className="btn btn-error">Pending</button>
+      </th>
+    </tr>
+  );
 };
 
 export default BookingRow;
